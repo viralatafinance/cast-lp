4322d3ae479be0c51e8e7651f086afd5327b3afa
DexREAU <andromedog2017@gmail.com>
2022-01-12 10:10:10 -0300

    v1

---
+++ main.js
@@ -0,0 +1,30 @@
+
+const loading = document.querySelector('#loading')
+
+document.onreadystatechange = function () {
+    if (document.readyState == "interactive" || document.readyState == "complete") {
+
+        loading.style.opacity = 0;
+        loading.style.visibility = 'hidden';
+
+    }
+}
+
+
+
+
+let windowPosition;
+const parallaxImg = document.querySelector('.parallax_img')
+
+if(window.matchMedia('(orientation: landscape) and (min-width:900px)').matches) {
+    document.body.onscroll = function() {
+        windowPosition = window.pageYOffset;
+        parallaxImg.style.transform = "translateY(" + windowPosition /2 + "px)";
+    }
+} else {
+    document.body.onscroll = function() {
+        windowPosition = window.pageYOffset;
+        parallaxImg.style.transform = "translateY( -" + windowPosition /2 + "px)";
+    }
+    
+}
