66b689ecdf30c3e54880e5b7b10778f77096ca7c
DexREAU <andromedog2017@gmail.com>
2022-01-13 11:25:43 -0300

    v6

--- main.js
+++ main.js
@@ -21,10 +21,31 @@ if(window.matchMedia('(orientation: landscape) and (min-width:900px)').matches) 
         windowPosition = window.pageYOffset;
         parallaxImg.style.transform = "translateY(" + windowPosition /2 + "px)";
     }
-} else {
-    document.body.onscroll = function() {
-        windowPosition = window.pageYOffset;
-        parallaxImg.style.transform = "translateY( -" + windowPosition /1.5 + "px)";
-    }
-    
 }
+
+
+function success() {
+    const button = document.querySelector('form button');
+    const inputs = document.querySelectorAll('form input');
+
+    button.innerText = "ENVIADO COM SUCESSO!"
+
+    for(i = 0; i < inputs.length; i++) {
+            inputs[i].value = ""
+    }
+
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+  let myForm = document.getElementById('top_form');
+  let formData = new FormData(myForm)
+  fetch('/', {
+    method: 'POST',
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams(formData).toString()
+  }).then(() => success()).catch((error) =>
+    alert(error))
+}
+
+document.getElementById('form').addEventListener("submit", handleSubmit);
